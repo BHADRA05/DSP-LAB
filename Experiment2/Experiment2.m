a7eac8874e23141e9f2bb7f87a4372d3f23b8d4c
BHADRA05 <122148966+BHADRA05@users.noreply.github.com>
2024-10-03 22:25:24 +0530

    Add files via upload

---
+++ Experiment2/Experiment2.m
@@ -0,0 +1,51 @@
+clc;
+clear all;
+close all;
+
+t = 0:0.01:1;
+fm = 10;
+y = sin(2*pi*fm*t);
+subplot(2,2,1);
+plot(t,y);
+hold on;
+stem(t,y);
+xlabel('time index');
+ylabel('amplitude');
+title('Original Signal');
+legend("Continuous","Discrete");
+
+fs1 = fm;
+t1 = 0:1/fs1:1;
+y1 = sin(2*pi*fm*t1);
+subplot(2,2,2);
+plot(t1,y1);
+hold on;
+stem(t1,y1);
+xlabel('time index');
+ylabel('amplitude');
+title('Undersampled Signal');
+legend("Continuous","Discrete");
+
+fs2 = 3*fm;
+t2 = 0:1/fs2:1;
+y2 = sin(2*pi*fm*t2);
+subplot(2,2,3);
+plot(t2,y2);
+hold on;
+stem(t2,y2);
+xlabel('time index');
+ylabel('amplitude');
+title('Nyquist sampled Signal');
+legend("Continuous","Discrete");
+
+fs3 = 10*fm;
+t3 = 0:1/fs3:1;
+y3 = sin(2*pi*fm*t3);
+subplot(2,2,4);
+plot(t3,y3);
+hold on;
+stem(t3,y3);
+xlabel('time index');
+ylabel('amplitude');
+title('Oversampled Signal');
+legend("Continuous","Discrete");
